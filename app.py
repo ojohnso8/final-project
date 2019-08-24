7d221f5c9ce929633ad5672b3e2154ce5942cc0b
ojohnso8 <46421819+ojohnso8@users.noreply.github.com>
2019-08-24 12:04:56 -0400

    final commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_static():
+    return render_template('index.html')
+
+@app.route('/index.html')
+def render_index():
+    return render_template('index.html')
+
+@app.route('/incidentmap.html')
+def render_incidentmap():
+    return render_template('incidentmap.html')
+
+@app.route('/markermap.html')
+def render_markermap():
+    return render_template('markermap.html')
+
+@app.route('/plots.html')
+def render_plots():
+    return render_template('plots.html')
+
+@app.route('/predictions.html')
+def render_predictions():
+    return render_template('predictions.html')
+
+@app.route('/temperature.html')
+def render_temperature():
+    return render_template('temperature.html')
+
+@app.route('/totaldead.html')
+def render_totaldead():
+    return render_template('totaldead.html')
+
+@app.route('/sourcequalitylinear.html')
+def render_sourcequalitylinear():
+    return render_template('sourcequalitylinear.html')
+
+@app.route('/sourcequalitylogistic.html')
+def render_sourcequalitylogistic():
+    return render_template('sourcequalitylogistic.html')
+
+@app.route('/data.html')
+def render_data():
+    return render_template('data.html')
+
+if __name__ == '__main__':
+    app.run()
